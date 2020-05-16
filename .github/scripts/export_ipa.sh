a0de1d44d2f899a138978051fde7152a7ced506e
Lucas Desouza <lucasfds2777@gmail.com>
2020-05-16 17:12:36 -0500

    trying a different command

--- .github/scripts/export_ipa.sh
+++ .github/scripts/export_ipa.sh
@@ -6,5 +6,4 @@ set -eo pipefail
 xcodebuild -archivePath $PWD/build/Count\ my\ breaths.xcarchive \
             -exportOptionsPlist Count\ my\ breaths/exportOptions.plist \
             -exportPath $PWD/build \
-            -allowProvisioningUpdates \
             -exportArchive | xcpretty
