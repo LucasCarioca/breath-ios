ac9a7443064644b4a4cf43a3f11a5dc1f02e2f27
Lucas Desouza <lucasfds2777@gmail.com>
2020-05-17 04:10:11 -0500

    Feat/fastlane (#3)
    * adding brewfile and lock
    
    * adding env file
    
    * adding fastlane
    
    * update fastlane and gh action
    
    * add increment by testflight version
    
    * update fastlane
    
    * remove bad method
    
    * update fastlane script
    
    * correct apple id
    
    * apple id string
    
    * adds ignore for report file
    
    * ignore report
    
    * Version Bump to 12
    
    * adding beta to action
    
    * check status
    
    * update workflow names and ignore uneded files
    
    * remove status step
    
    * status
    
    * remove brew lock
    
    * adds env for fastlane password
    
    * fastlane password
    
    * testing alternative auth
    
    * adds git login
    
    * fix step
    
    * fix login
    
    * push changes separately
    
    * push git remote update
    
    * check stuff
    
    * adds checkout to allow pushing
    
    * adds script to fix branch
    
    * adds user info
    
    * Version Bump to 20
    
    * adds release

---
+++ env.sh
@@ -0,0 +1,33 @@
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+git_setup() {
+    cat <<-EOF >$HOME/.netrc
+        machine github.com
+        login $GITHUB_ACTOR
+        password $GITHUB_TOKEN
+        machine api.github.com
+        login $GITHUB_ACTOR
+        password $GITHUB_TOKEN
+EOF
+    chmod 600 $HOME/.netrc
+    git config --global user.email "$INPUT_AUTHOR_EMAIL"
+    git config --global user.name "$INPUT_AUTHOR_NAME"
+}
+
+git_setup
+
+git fetch
+
+# Verify if the branch needs to be created
+if ! git rev-parse --verify --quiet "$GITHUB_REF"; then
+    echo "Creating branch..."
+    git branch "$GITHUB_REF"
+fi
+
+# Switch to branch from current workflow run
+echo "Switching branch..."
+git checkout "$GITHUB_REF"
+
+git config --local user.name "Lucas Desouza"
+git config --local user.email "lucasfds2777@gmail.com"
