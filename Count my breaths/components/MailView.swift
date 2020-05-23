68cf7ce8ee6aa4fb8975081c9b1b1943bfce4f18
Lucas Desouza <lucasfds2777@gmail.com>
2020-05-23 02:54:51 -0500

    Adds export to email support (#5)
    * adds email export of data
    
    * fix ci job
    
    * updates version to trouble shoot issues
    
    * fix missing file issue
    
    * fix chart view version issue
    
    * adds missing files
    
    * adds missing files
    
    * Version Bump to 33

---
+++ Count my breaths/components/MailView.swift
@@ -0,0 +1,79 @@
+//
+// Created by Lucas Desouza on 5/23/20.
+// Copyright (c) 2020 Lucas Desouza. All rights reserved.
+//
+
+import MessageUI
+import SwiftUI
+
+struct MailButtonView: View {
+
+    @State var result: Result<MFMailComposeResult, Error>? = nil
+    @State var isShowingMailView = false
+    @State var csvData: Data? = nil
+
+    var body: some View {
+        VStack {
+            if MFMailComposeViewController.canSendMail() {
+                Button("Email records") {
+                    self.isShowingMailView.toggle()
+                }
+            }
+        }.sheet(isPresented: $isShowingMailView) {
+            MailView(isShowing: self.$isShowingMailView, result: self.$result, csv: self.$csvData)
+        }
+    }
+}
+
+struct MailView: UIViewControllerRepresentable {
+
+    @Binding var isShowing: Bool
+    @Binding var result: Result<MFMailComposeResult, Error>?
+    @Binding var csv: Data?
+
+    class Coordinator: NSObject, MFMailComposeViewControllerDelegate {
+
+        @Binding var isShowing: Bool
+        @Binding var result: Result<MFMailComposeResult, Error>?
+        @Binding var csv: Data?
+
+        init(isShowing: Binding<Bool>,
+             result: Binding<Result<MFMailComposeResult, Error>?>, csv: Binding<Data?>) {
+            _isShowing = isShowing
+            _result = result
+            _csv = csv
+        }
+
+        func mailComposeController(_ controller: MFMailComposeViewController,
+                                   didFinishWith result: MFMailComposeResult,
+                                   error: Error?) {
+            defer {
+                isShowing = false
+            }
+            guard error == nil else {
+                self.result = .failure(error!)
+                return
+            }
+            self.result = .success(result)
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(isShowing: $isShowing,
+                result: $result, csv: $csv)
+    }
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<MailView>) -> MFMailComposeViewController {
+        let vc = MFMailComposeViewController()
+        vc.mailComposeDelegate = context.coordinator
+        if self.csv != nil {
+            vc.addAttachmentData(self.csv!, mimeType: "text/csv", fileName: "breaths-per-minute.csv")
+        }
+        return vc
+    }
+
+    func updateUIViewController(_ uiViewController: MFMailComposeViewController,
+                                context: UIViewControllerRepresentableContext<MailView>) {
+
+    }
+}
