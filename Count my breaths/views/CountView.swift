5b9689d74d18a59e8547063ee0e614e6f92da590
Lucas Desouza <lucasfds2777@gmail.com>
2020-07-28 23:28:28 -0500

    refactor project

--- Count my breaths/views/CountView.swift
+++ Count my breaths/views/CountView.swift
@@ -13,7 +13,6 @@ import QuickComponents
 struct CountView: View {
     @Environment(\.colorScheme) var colorScheme
     @Environment(\.managedObjectContext) var managedObjectContext
-
     @FetchRequest(fetchRequest: requestBuilder(limit: 1, sort: [])) var countRecords: FetchedResults<CountRecord>
 
     @State var timePublisher = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
@@ -23,85 +22,130 @@ struct CountView: View {
     @State var showResults = false
     @State var bpm: Int = 0
     @State var warning = false
+    
     let impactMed = UIImpactFeedbackGenerator(style: .medium)
     let impactHeavy = UIImpactFeedbackGenerator(style: .heavy)
     let hapticNotification = UINotificationFeedbackGenerator()
     
-
     var body: some View {
         VStack {
-            HeaderView(title: "Counter", subTitle: "")
+            Text("Counter").Heading(size: .H3)
             Text("Breaths: " + String(self.counter)).font(.title)
                 .font(.subheadline)
-            BarView(value: CGFloat(self.timer), max: 30).frame(height:50)
-                .onReceive(self.timePublisher) { time in
-                    if(self.isCounting) {
-                        if (self.timer >= 30) {
-                            self.isCounting = false
-                            self.showResults = true
-                            self.bpm = self.counter * 2
-                            if self.bpm >= 30 {
-                                self.warning = true
-                                self.hapticNotification.notificationOccurred(.error)
-                            } else {
-                                self.hapticNotification.notificationOccurred(.success)
-                            }
-                            let record = CountRecord(context: self.managedObjectContext)
-                            record.elapsedTime = Int16(self.timer)
-                            record.beats = Int16(self.counter)
-                            record.time = Date(timeIntervalSinceReferenceDate: time.timeIntervalSinceReferenceDate)
-                            let df = DateFormatter()
-                            df.dateFormat = "MM-dd-yyyy hh:mm:ss"
-                            record.timeText = df.string(from: record.time ?? Date())
-                            do{
-                               try self.managedObjectContext.save()
-                            } catch {
-                                print("Failed to save")
-                            }
-                            let userDefaults = UserDefaults.standard
-                            let countRuns = userDefaults.integer(forKey: "countruns")
-                            userDefaults.set(countRuns+1, forKey: "countruns")
-                            self.timer = 0
-                        } else {
-                            self.timer += 1
-                        }
-                    }
-            }
             self.showResults ?
                 Text("Counted " + String(self.bpm) + " beats per minute.") : nil
             self.isCounting ?
-                Text("Seconds remaining \(30-self.timer)") : nil
+                TimerView(time: self.timer, max: 30) : nil
+            Spacer()
             
-            Spacer()
-            Spacer()
-            Button(action: {
-                self.impactMed.impactOccurred()
-                if (!self.isCounting) {
-                    self.timePublisher = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-                    self.counter = 1
-                    self.isCounting = true
-                    self.timer = 0
-                } else {
-                    self.counter += 1
-                }
-            }) {
+            
+            Button(action: self.breath) {
                 GeometryReader { geometry in
-                    
-                    Image("icon").frame(width: geometry.size.width, height: geometry.size.height).background(Color(red:78/255, green:78/255, blue:78/255)).cornerRadius(20)
+                    Image("icon")
+                        .frame(width: geometry.size.width, height: geometry.size.height)
+                        .background(Color(red:78/255, green:78/255, blue:78/255))
+                        .cornerRadius(20)
                 }
             }.buttonStyle(PlainButtonStyle())
-            self.isCounting ?
-                    Button(action: {
-                        self.isCounting = false
-                        self.showResults = false
-                        self.counter = 0
-                        self.timer = 0
-                    }){
-                        Text("Reset")
-                    }.frame(height:50).buttonStyle(SecondaryButton()) : nil
+            .onReceive(self.timePublisher) { time in
+                if(self.isCounting) {
+                    if (self.timer >= 30) {
+                        self.saveRecord(timeInterval: time.timeIntervalSinceReferenceDate)
+                        self.finishCounting()
+                    } else {
+                        self.timer += 1
+                    }
+                }
+            }
+
+            self.isCounting ? Button(action: self.reset){
+                    Text("Reset")
+            }.frame(height:50).buttonStyle(SecondaryButton()) : nil
             Spacer()
+            
         }.alert(isPresented: self.$warning) {
-            Alert(title: Text("Your pets breathing is high"), message: Text("Please contact your veterinarian."), dismissButton: .default(Text("Ok")))
+            Alert(
+                title: Text("Your pets breathing is high"),
+                message: Text("Please contact your veterinarian."),
+                dismissButton: .default(Text("Ok")
+                )
+            )
+        }    }
+    
+    func reset() {
+        self.isCounting = false
+        self.counter = 0
+        self.timer = 0
+    }
+    
+    func finishCounting() {
+        calculateResults()
+        if self.bpm >= 30 {
+            highBreathing()
+        } else {
+            normalBreathing()
+        }
+        reset()
+        trackRuns()
+    }
+    
+    func calculateResults() {
+        self.bpm = self.counter * 2
+        self.showResults = true
+    }
+    
+    func saveRecord(timeInterval: TimeInterval) {
+        let record = CountRecord(context: self.managedObjectContext)
+        record.elapsedTime = Int16(self.timer)
+        record.beats = Int16(self.counter)
+        record.time = Date(timeIntervalSinceReferenceDate: timeInterval)
+        let df = DateFormatter()
+        df.dateFormat = "MM-dd-yyyy hh:mm:ss"
+        record.timeText = df.string(from: record.time ?? Date())
+        do{
+           try self.managedObjectContext.save()
+        } catch {
+            print("Failed to save")
+        }
+    }
+    
+    func trackRuns() {
+        let userDefaults = UserDefaults.standard
+        let countRuns = userDefaults.integer(forKey: "countruns")
+        userDefaults.set(countRuns+1, forKey: "countruns")
+    }
+    
+    func highBreathing() {
+        self.warning = true
+        self.hapticNotification.notificationOccurred(.error)
+    }
+    
+    func normalBreathing() {
+        self.hapticNotification.notificationOccurred(.success)
+    }
+    
+    func breath() {
+        self.impactMed.impactOccurred()
+        if (!self.isCounting) {
+            self.timePublisher = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+            self.counter = 1
+            self.isCounting = true
+            self.timer = 0
+        } else {
+            self.counter += 1
+        }
+    }
+}
+
+struct TimerView: View {
+    
+    var time: Int
+    var max: Int
+    
+    var body: some View {
+        VStack{
+            BarView(value: CGFloat(time), max: CGFloat(max)).frame(height:50)
+            Text("Seconds remaining \(max-time)")
         }
     }
 }
