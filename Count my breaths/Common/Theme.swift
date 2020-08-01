44d3eff3ff9d32cd23243912e01491fe52d7fdb0
Lucas Desouza <lucasfds2777@gmail.com>
2020-08-01 14:10:47 -0500

    adding common

---
+++ Count my breaths/Common/Theme.swift
@@ -0,0 +1,25 @@
+//
+//  Theme.swift
+//  Count my breaths
+//
+//  Created by Lucas Desouza on 8/1/20.
+//  Copyright © 2020 Lucas Desouza. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+public class Theme {
+    static let colors = Colors()
+}
+
+class Colors {
+    let primary = Color(#colorLiteral(red: 0.2739317119, green: 0.7950076461, blue: 0.9080155492, alpha: 1))
+    let secondary = Color(#colorLiteral(red: 0.9019607843, green: 0.5803921569, blue: 0.6901960784, alpha: 1))
+    let text = Color(#colorLiteral(red: 0.9960784314, green: 0.9803921569, blue: 0.9764705882, alpha: 1))
+    let background = Color(#colorLiteral(red: 0.3058487773, green: 0.3059058487, blue: 0.3058412671, alpha: 1))
+    let primaryRaw = "#46CBE8"
+    let secondaryRaw = "#E694B0"
+    let textRaw = "#FEFAF9"
+    let backgroundRaw = "#4E4E4E"
+}
