ea24ea8316ee74721f668a94de42a366b16965be
Lucas Desouza <lucasfds2777@gmail.com>
2020-08-20 00:09:04 -0500

    Feat/list view date range (#46)
    * Version Bump to 1
    
    * feat(history): adds the date range picker to the list view
    
    * v1.17.0-beta2
    
    * fix(history): fixes some cosmetic stuff
    
    * v1.17.0-beta3
    
    * fix(history): fix bug that caused ranges not to work
    
    * v1.17.0-beta4
    
    * feat(history): improve date formatting
    
    * v1.17.0-beta5
    
    * feat(history): move date to above the bar
    
    * v1.17.0-beta6
    
    * fix(history): adds missing date ranges
    
    * v1.17.0-beta7

--- Count my breaths/components/RecordView.swift
+++ Count my breaths/components/RecordView.swift
@@ -8,28 +8,34 @@ import QuickComponents
 
 struct RecordView: View {
     var beats: Int16
-    var timeText: String
+    var time: Date
     var targetBpm: Int
 
     var body: some View {
         VStack{
+            HStack {
+                Text(getDate()).font(.custom("", size: 18))
+                Spacer()
+            }
             BarView(
                 value: CGFloat(beats * 2),
                 max: CGFloat(targetBpm + 10),
                 showLabel: true,
                 color: ((beats * 2) >= targetBpm) ? Theme.colors.secondary : Theme.colors.primary)
-                .padding(.top, 10)
-                .padding(.bottom, 35)
-            HStack {
-                Text(timeText).font(.subheadline)
-                Spacer()
-            }
+            .frame(height: 50)
         }
     }
+    
+    func getDate() -> String{
+        let dateFormatterPrint = DateFormatter()
+        dateFormatterPrint.dateFormat = "MMM dd,yyyy HH:mm"
+        return dateFormatterPrint.string(from: self.time)
+    }
+
 }
 
 struct RecordView_Previews: PreviewProvider {
     static var previews: some View {
-        RecordView(beats: 20, timeText: "HelloWorld", targetBpm: 30)
+        RecordView(beats: 20, time: Date(), targetBpm: 30)
     }
 }
