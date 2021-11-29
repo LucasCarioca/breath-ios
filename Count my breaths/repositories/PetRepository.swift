9f39690f1f44c2cc27bfbdb3de51ddadd9e12984
Lucas Desouza <lucasfds2777@gmail.com>
2021-11-28 23:31:49 -0600

    chore: create pet repo and proces to migrate from defaults

---
+++ Count my breaths/repositories/PetRepository.swift
@@ -0,0 +1,137 @@
+//
+// Created by Lucas Desouza on 11/28/21.
+// Copyright (c) 2021 Lucas Desouza. All rights reserved.
+//
+
+import Foundation
+import Foundation
+import CoreData
+import SwiftUI
+
+struct PetRepositoryKey: EnvironmentKey {
+    static let defaultValue = PetRepository()
+}
+
+extension EnvironmentValues {
+    var petRepository: PetRepository {
+        get {
+            self[PetRepositoryKey.self]
+        }
+        set {
+            self[PetRepositoryKey.self] = newValue
+        }
+    }
+}
+
+class PetRepository {
+    private var ctx: NSManagedObjectContext?
+    private var countRecordRepository: CountRecordRepository?
+
+    init() {}
+
+    init(ctx: NSManagedObjectContext) {
+        self.ctx = ctx
+        countRecordRepository = CountRecordRepository(ctx: ctx)
+    }
+
+    public func setCtx(ctx: NSManagedObjectContext) {
+        self.ctx = ctx
+        countRecordRepository = CountRecordRepository(ctx: ctx)
+    }
+
+    public func delete(_ item: Pet) {
+        do {
+            guard let ctx = ctx else {
+                throw NSError()
+            }
+            ctx.delete(item)
+            try ctx.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+    }
+
+    //temporary to migrate data from user defaults to core data
+    public func createFromDefault() -> Pet {
+        print("creating pet from defaults")
+        let petProfile = PetProfileController.loadPetProfile()
+        let pet = findByName(petProfile.name)
+        if let pet = pet {
+            print("pet already exists, skipping ...")
+            return pet
+        }
+        do {
+            guard let ctx = ctx else {
+                throw NSError()
+            }
+            let pet = Pet(context: ctx)
+            pet.name = petProfile.name
+            pet.chipId = petProfile.chipId
+            pet.targetBreathing = Double(petProfile.targetBpm)
+            let breathRecords = countRecordRepository?.getAllByPet()
+            if let records = breathRecords {
+                for record in records {
+                    pet.addToBreathRecords(record)
+                }
+            }
+            try ctx.save()
+            print("new pet created and records associated")
+            return pet
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+    }
+
+
+    public func create(
+            name: String = "myPet",
+            breed: String? = nil,
+            birthDate: Date? = nil,
+            chipId: String? = nil,
+            breathRecords: [CountRecord]? = [],
+            targetBreathing: Double = 30)-> Pet {
+        let petProfile = PetProfileController.loadPetProfile()
+        let pet = findByName(petProfile.name)
+        if let pet = pet {
+            return pet
+        }
+        do {
+            guard let ctx = ctx else {
+                throw NSError()
+            }
+            let pet = Pet(context: ctx)
+            pet.name = name
+            pet.breed = breed
+            pet.birthdate = birthDate
+            pet.chipId = chipId
+            pet.targetBreathing = targetBreathing
+            if let records = breathRecords {
+                for record in records {
+                    pet.addToBreathRecords(record)
+                }
+            }
+            try ctx.save()
+            print("new pet created and records associated")
+            return pet
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+    }
+
+    public func findByName(_ name: String) -> Pet? {
+        let request = Pet.fetchRequest()
+        request.predicate = NSPredicate(format: "name == %@", name)
+        do {
+            guard let ctx = ctx else {
+                throw NSError()
+            }
+            return try ctx.fetch(request).first
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+    }
+}
