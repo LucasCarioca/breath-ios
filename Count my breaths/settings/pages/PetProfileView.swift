8179466e93b75e03bdc1ca2a3bd9179f64a5b529
Lucas Desouza <lucasfds2777@gmail.com>
2021-12-01 00:12:56 -0600

    fix: remove vstack causing white bar on profile page

--- Count my breaths/settings/pages/PetProfileView.swift
+++ Count my breaths/settings/pages/PetProfileView.swift
@@ -12,32 +12,29 @@ struct PetProfileView: View {
     @State var petProfile = PetProfileController.loadPetProfile()
     @State var pet: Pet
     var body: some View {
-        VStack {
-            List {
-                NavigationLink(destination: TargetBreathingRateView(targetBpm: petProfile.targetBpm, action: updateTargetBpm)) {
-                    HStack {
-                        Image(systemName: "timer")
-                        Text("Target breathing rate: ")
-                        Text("\(String(format: "%.0f", pet.targetBreathing ?? 30))").fontWeight(.heavy)
-                        Spacer()
-                    }
-                }
-                NavigationLink(destination: PetProfileTextFieldView(label: "Pet name", name: petProfile.name, action: updateName)) {
-                    HStack {
-                        Text("Pet name: ")
-                        Text(pet.name ?? "Missing pet name").fontWeight(.heavy)
-                        Spacer()
-                    }
-                }
-                NavigationLink(destination: PetProfileTextFieldView(label: "Chip Id", name: petProfile.chipId, action: updateChipId)) {
-                    HStack {
-                        Text("Chip Id: ")
-                        Text(pet.chipId ?? "Missing chip id").fontWeight(.heavy)
-                        Spacer()
-                    }
+        List {
+            NavigationLink(destination: TargetBreathingRateView(targetBpm: petProfile.targetBpm, action: updateTargetBpm)) {
+                HStack {
+                    Image(systemName: "timer")
+                    Text("Target breathing rate: ")
+                    Text("\(String(format: "%.0f", pet.targetBreathing ?? 30))").fontWeight(.heavy)
+                    Spacer()
                 }
             }
-            Spacer()
+            NavigationLink(destination: PetProfileTextFieldView(label: "Pet name", name: petProfile.name, action: updateName)) {
+                HStack {
+                    Text("Pet name: ")
+                    Text(pet.name ?? "Missing pet name").fontWeight(.heavy)
+                    Spacer()
+                }
+            }
+            NavigationLink(destination: PetProfileTextFieldView(label: "Chip Id", name: petProfile.chipId, action: updateChipId)) {
+                HStack {
+                    Text("Chip Id: ")
+                    Text(pet.chipId ?? "Missing chip id").fontWeight(.heavy)
+                    Spacer()
+                }
+            }
         }
     }
 
